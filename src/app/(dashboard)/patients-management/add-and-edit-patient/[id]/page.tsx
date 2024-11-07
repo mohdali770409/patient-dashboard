e6e4c33adc0cb6fe001276c0f69865e7e0007ddf
Mohd Ali <mohammad.ali@nextlabsonline.com>
2024-11-07 16:36:13 +0530

    loader added

--- src/app/(dashboard)/patients-management/add-and-edit-patient/[id]/page.tsx
+++ src/app/(dashboard)/patients-management/add-and-edit-patient/[id]/page.tsx
@@ -25,7 +25,11 @@ const AddAndEditPatients = ({params}:{params:{id:string}}) => {
   }, [params.id])
 
   if (isLoading) {
-    return <div>Loading patient data...</div>
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
+    );
   }
 
 
